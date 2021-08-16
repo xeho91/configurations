c2b1f5e528924a61fb2c15ed214a40bc5f19b479
xeho91 <xeho91@pm.me>
2021-08-16 13:21:41 +0800

    build(library): 🏗️ Add `helpers/` and `has-module`
    Created a new directory `library/` for modules which are going to be
    reusable.
    
    Created a new part of `library/` - `helpers/`.
    
    Added a new helper - `hasModule(moduleName)` in the file `has-module`.

---
+++ library/helpers/source/has-module.ts
@@ -0,0 +1,10 @@
+export function hasModule(name: string): boolean {
+	try {
+		/* eslint-disable-next-line unicorn/prefer-module */
+		require.resolve(name);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
